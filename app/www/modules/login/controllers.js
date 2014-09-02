52939af3ecaf2d5629272ab4f057344bbfb397d4
Tyler <tyhlewis@gmail.com>
2014-09-02 16:28:30 -0400

    Add Facebook SDK, add FB login button to homepage, delete unnecessary code in controllers and services

--- app/www/modules/login/controllers.js
+++ app/www/modules/login/controllers.js
@@ -1,15 +1,7 @@
-angular.module('starter.controllers', [])
+angular.module('closebuy.login.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('loginController', function($scope, Login) {
+  $scope.loginUser = function() {
+    Login.loginUser();
+  };
 });
