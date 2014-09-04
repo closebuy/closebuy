08785fa0e423201d84c0993b66b834fdac2d308a
Tyler <tyhlewis@gmail.com>
2014-09-04 11:27:03 -0400

    Add login/controllers changes

--- app/www/modules/login/controllers.js
+++ app/www/modules/login/controllers.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 angular.module('login', ['login.services'])
-=======
->>>>>>> 9976ab40ec7032767b45f04c2f8162a318d9e198
 
 .controller('LoginController', function($scope, Auth) {
   $scope.existingUser = {};
@@ -19,6 +16,4 @@ angular.module('login', ['login.services'])
   $scope.logoutUser = function() {
     Auth.logoutUser();
   };
-<<<<<<< HEAD
 });
->>>>>>> 9976ab40ec7032767b45f04c2f8162a318d9e198
